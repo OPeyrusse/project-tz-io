dfbc855c3defa0d4000db39c083940fe3fa6b317
Kineolyan <kineolyan@gmail.com>
2018-02-10 14:18:31 +0100

    Code partial mov operation.

--- rs-parser/src/parser/instructions.rs
+++ rs-parser/src/parser/instructions.rs
@@ -1,7 +1,8 @@
-use nom::{space, newline as nl};
+use nom::space;
 
 use parser::common::{RawData, be_uint, be_u8};
 
+// Value pointers parsing
 #[derive(Debug, PartialEq)]
 pub enum ValuePointer {
   VALUE(u32),
@@ -9,32 +10,70 @@ pub enum ValuePointer {
   PORT(u32)
 }
 
+named!(acc_pointer<&RawData, ValuePointer>,
+  do_parse!(
+    tag!("ACC") >>
+    idx: be_u8 >>
+    (ValuePointer::ACC(idx))
+  )
+);
+named!(input_port<&RawData, ValuePointer>,
+  do_parse!(
+    port: be_uint >>
+    tag!(">") >>
+    (ValuePointer::PORT(port))
+  )
+);
+named!(output_port<&RawData, ValuePointer>,
+  do_parse!(
+    tag!(">") >>
+    port: be_uint >>
+    (ValuePointer::PORT(port))
+  )
+);
+named!(value_pointer<&RawData, ValuePointer>,
+  map!(be_uint, |value| ValuePointer::VALUE(value))
+);
+
+// Operation parsing
 #[derive(Debug, PartialEq)]
 pub enum Operation {
   MOV(ValuePointer, ValuePointer)
 }
 
-named!(acc_pointer,
-  do_parse!(
-    tag!("ACC") >>
-    idx: map!(opt!(be_u8), |value| value.unwrap_or(1u8)) >>
-    (ValuePointer::ACC(idx))
-  )
-);
-
-named!(mov<&RawData, Operation>,
+named!(mov_from_in<&RawData, Operation>,
   do_parse!(
     tag!("MOV") >> space >>
-    from: be_uint >> 
-    tag!(">") >>
-    space >> tag!("->") >> space >>
-    to: acc_pointer >>
-    (Operation::MOV(ValuePointer::PORT(from), to))
+    from: input_port >>
+    opt!(space) >> tag!(",") >> opt!(space) >>
+    to: alt!(acc_pointer | output_port) >>
+    (Operation::MOV(from, to))
+  )
+);
+named!(mov_operation<&RawData, Operation>,
+  alt!(
+    mov_from_in |
+    do_parse!(
+      // MOV from anything to output, but input
+      tag!("MOV") >> space >>
+      from: alt!(acc_pointer | value_pointer) >>
+      space >> tag!(",") >> space >>
+      to: output_port >>
+      (Operation::MOV(from, to))
+    ) |
+    do_parse!(
+      // MOV value to acc
+      tag!("MOV") >> space >>
+      from: value_pointer >>
+      space >> tag!(",") >> space >>
+      to: acc_pointer >>
+      (Operation::MOV(from, to))
+    )
   )
 );
 
 named!(pub parse_instruction<&RawData, Operation>,
-  alt!(mov | mov)
+  alt!(mov_operation | mov_operation)
 );
 
 #[cfg(test)]
@@ -42,11 +81,81 @@ mod tests {
   use super::*;
   use parser::common::tests::assert_full_result;
 
-  fn test_parse_acc_pointer() {
-    let res_implicit = acc_pointer(b"ACC");
-    assert_full_result(res_implicit, ValuePointer::ACC(1));
-
+  #[test]
+  fn test_parse_acc_pointer_explicit() {
     let res_explicit = acc_pointer(b"ACC3");
     assert_full_result(res_explicit, ValuePointer::ACC(3));
   }
+
+  #[test]
+  fn test_parse_acc_pointer_implicit() {
+    let res_implicit = acc_pointer(b"ACC1");
+    assert_full_result(res_implicit, ValuePointer::ACC(1));
+  }
+
+  #[test]
+  fn test_parse_input_port() {
+    let res = input_port(b"12>");
+    assert_full_result(res, ValuePointer::PORT(12));
+  }
+
+  #[test]
+  fn test_parse_output_port() {
+    let res = output_port(b">43");
+    assert_full_result(res, ValuePointer::PORT(43));
+  }
+
+  #[test]
+  fn test_parse_value_pointer() {
+    let res = value_pointer(b"37");
+    assert_full_result(res, ValuePointer::VALUE(37u32));
+  }
+
+  #[test]
+  fn test_parse_mov_in_to_out() {
+    let res = mov_operation(b"MOV 1>, >2");
+    assert_full_result(
+      res,
+      Operation::MOV(
+        ValuePointer::PORT(1),
+        ValuePointer::PORT(2)
+      )
+    );
+  }
+
+  // #[test]
+  // fn test_parse_mov_value_to_out() {
+  //   let res = mov_operation(b"MOV 12, >3");
+  //   assert_full_result(
+  //     res,
+  //     Operation::MOV(
+  //       ValuePointer::VALUE(12),
+  //       ValuePointer::PORT(3)
+  //     )
+  //   );
+  // }
+
+  // #[test]
+  // fn test_parse_mov_value_to_acc() {
+  //   let res = mov_operation(b"MOV 45, ACC2");
+  //   assert_full_result(
+  //     res,
+  //     Operation::MOV(
+  //       ValuePointer::VALUE(45),
+  //       ValuePointer::ACC(2)
+  //     )
+  //   );
+  // }
+
+  // #[test]
+  // fn test_parse_mov_in_to_acc() {
+  //   let res = mov_operation(b"MOV 76, ACC1");
+  //   assert_full_result(
+  //     res,
+  //     Operation::MOV(
+  //       ValuePointer::VALUE(76),
+  //       ValuePointer::ACC(1)
+  //     )
+  //   );
+  // }
 }
