78a96f6661568e8f43a757103c13a74afc202744
Olivier Peyrusse <ope@activeviam.com>
2018-04-21 10:57:18 +0200

    More tests on operations.

---
+++ tzio-core/src/test/java/com/kineolyan/tzio/v1/ops/TestIncrementOperation.java
@@ -0,0 +1,40 @@
+package com.kineolyan.tzio.v1.ops;
+
+import com.kineolyan.tzio.v1.Node;
+import com.kineolyan.tzio.v1.ref.AccReference;
+import com.kineolyan.tzio.v1.ref.ValueReference;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Kineolyan
+ */
+public class TestIncrementOperation {
+
+	private Node node;
+
+	@BeforeEach
+	void createNode() {
+		this.node = OperationTestUtil.defaultNode();
+	}
+
+	@Test
+	void testAddOperationWithValue() {
+		this.node.setAccValue(54);
+		final Operation.Shift shift = Operations.ADD(ValueReference.of(46)).execute(this.node);
+
+		Assertions.assertThat(this.node.getAccValue()).isEqualTo(100);
+		OperationTestUtil.assertThat(shift).shiftToNext();
+	}
+
+	@Test
+	void testAddOperationWithAcc() {
+		this.node.setAccValue(35);
+		final Operation.Shift shift = Operations.ADD(AccReference.INSTANCE).execute(this.node);
+
+		Assertions.assertThat(this.node.getAccValue()).isEqualTo(70);
+		OperationTestUtil.assertThat(shift).shiftToNext();
+	}
+
+}
