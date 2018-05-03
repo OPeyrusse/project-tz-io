33fb530a4128fc95550249b6116d4b4b58f26e2f
Kineolyan <kineolyan@gmail.com>
2018-05-03 02:04:56 +0200

    Correctly compute the length of operations and attributes.

--- rs-parser/src/generator/java/writer.rs
+++ rs-parser/src/generator/java/writer.rs
@@ -30,124 +30,160 @@ static VERSIONS: [u8; 4] = [/* minor */0, 0, /* major */0, 52];
 //    };
 // }
 
-fn write_u8(file: &mut File, value: u8) -> io::Result<()> {
-  let buf: [u8; 1] = [value];
-  file.write_all(&buf)
+type StdResult = io::Result<()>;
+
+trait Writer {
+  fn write(&mut self, data: &[u8]) -> StdResult;
 }
 
-fn write_u16(file: &mut File, value: u16) -> io::Result<()> {
+struct FileWriter {
+  file: File
+}
+
+impl FileWriter {
+  fn flush(&mut self) -> StdResult {
+    self.file.flush()
+  }
+}
+
+impl Writer for FileWriter {
+  fn write(&mut self, data: &[u8]) -> StdResult {
+    self.file.write_all(data)
+  }
+}
+
+struct VecWriter {
+  pub data: Vec<u8>
+}
+
+impl Writer for VecWriter {
+  fn write(&mut self, data: &[u8]) -> StdResult {
+    self.data.reserve_exact(data.len());
+    for byte in data {
+      self.data.push(*byte);
+    }
+    Ok(())
+  }
+}
+
+fn write_u8(writer: &mut Writer, value: u8) -> StdResult {
+  let buf: [u8; 1] = [value];
+  writer.write(&buf)
+}
+
+fn write_u16(writer: &mut Writer, value: u16) -> StdResult {
   let buf: [u8; 2] = [
     (value >> 8) as u8,
-    (value & 0xff) as u8 
+    (value & 0xff) as u8
   ];
-  file.write_all(&buf)
+  writer.write(&buf)
 }
 
-fn write_u32(file: &mut File, value: u32) -> io::Result<()> {
+fn write_u32(writer: &mut Writer, value: u32) -> StdResult {
   let buf: [u8; 4] = [
     (value >> 24) as u8,
     ((value >> 16) & 0xff) as u8,
     ((value >> 8) & 0xff) as u8,
-    (value & 0xff) as u8 
+    (value & 0xff) as u8
   ];
-  file.write_all(&buf)
+  writer.write(&buf)
 }
 
-fn write_string(file: &mut File, value: &String) -> io::Result<()> {
+fn write_string(writer: &mut Writer, value: &String) -> StdResult {
   let bytes = value.as_bytes();
   if !bytes.iter().all(|c| 0 < *c && *c < 128) {
     panic!("Unsupported chars in the string: `{}`", value);
-  } 
-  file.write_all(bytes)
+  }
+  writer.write(bytes)
 }
 
-fn write_header(file: &mut File) -> io::Result<()> {
-  file.write_all(&MAGIC)?;
-  file.write_all(&VERSIONS)?;
-  file.flush()
+fn write_header(writer: &mut Writer) -> StdResult {
+  writer.write(&MAGIC)?;
+  writer.write(&VERSIONS)
 }
 
-fn write_constant_pool(file: &mut File, class: &JavaClass) -> io::Result<()> {
+fn write_constant_pool(writer: &mut Writer, class: &JavaClass) -> StdResult {
   let pool_size = class.pool_size();
-  write_u16(file, pool_size)?;
+  write_u16(writer, pool_size)?;
 
   for (_idx, element) in class.pool_iter() {
     match element {
       &PoolElement::Utf8Value(ref value) => {
-        write_u8(file, constants::PoolCode::Utf8 as u8)?;
-        write_u16(file, value.len() as u16)?;
-        write_string(file, value)?;
+        write_u8(writer, constants::PoolCode::Utf8 as u8)?;
+        write_u16(writer, value.len() as u16)?;
+        write_string(writer, value)?;
       },
       &PoolElement::ClassInfo(c_idx) => {
-        write_u8(file, constants::PoolCode::Class as u8)?;
-        write_u16(file, c_idx)?;
+        write_u8(writer, constants::PoolCode::Class as u8)?;
+        write_u16(writer, c_idx)?;
       },
       &PoolElement::Integer(value) => {
-        write_u8(file, constants::PoolCode::Integer as u8)?;
-        write_u32(file, value)?;
+        write_u8(writer, constants::PoolCode::Integer as u8)?;
+        write_u32(writer, value)?;
       },
       &PoolElement::MethodRef(class_idx, name_idx) => {
-        write_u8(file, constants::PoolCode::MethodRef as u8)?;
-        write_u16(file, class_idx)?;
-        write_u16(file, name_idx)?;
+        write_u8(writer, constants::PoolCode::MethodRef as u8)?;
+        write_u16(writer, class_idx)?;
+        write_u16(writer, name_idx)?;
       },
       &PoolElement::NameAndType(name_idx, descriptor_idx) => {
-        write_u8(file, constants::PoolCode::NameAndType as u8)?;
-        write_u16(file, name_idx)?;
-        write_u16(file, descriptor_idx)?;
+        write_u8(writer, constants::PoolCode::NameAndType as u8)?;
+        write_u16(writer, name_idx)?;
+        write_u16(writer, descriptor_idx)?;
       }
-    } 
-  }
-  file.flush()
-}
-
-fn write_class_info(file: &mut File, class: &JavaClass) -> io::Result<()> {
-  let access: u16 = (constants::ClassAccess::FINAL as u16)
-    | (constants::ClassAccess::SUPER as u16);
-  write_u16(file, access)?;
-  write_u16(file, class.class_id)?;
-  write_u16(file, class.super_class_id)?;
-
-  // For now, tell that there are no interfaces
-  write_u16(file, class.interfaces.len() as u16)?;
-  for interface_id in &class.interfaces {
-    write_u16(file, *interface_id)?;
-  }
-  file.flush()
-}
-
-fn write_class_definition(file: &mut File, class: &JavaClass) -> io::Result<()> {
-  // TODO write the correct writer
-  // No fields
-  write_u16(file, 0)?;
-
-  // Write methods
-  write_u16(file, class.methods.len() as u16)?;
-  for method in &class.methods {
-    write_u16(file, method.access)?;
-    write_u16(file, method.name_index)?;
-    write_u16(file, method.descriptor_index)?;
-    write_u16(file, method.attributes.len() as u16)?;
-    for entry in &method.attributes {
-      write_attribute(file, entry)?;
     }
   }
-  
-  // No attributes
-  write_u16(file, 0)?;
-
-  file.flush()
+  Ok(())
 }
 
-fn write_attribute(file: &mut File, &(ref idx, ref attribute): &(u16, Attribute)) -> io::Result<()> {
+fn write_class_info(writer: &mut Writer, class: &JavaClass) -> StdResult {
+  let access: u16 = (constants::ClassAccess::FINAL as u16)
+    | (constants::ClassAccess::SUPER as u16);
+  write_u16(writer, access)?;
+  write_u16(writer, class.class_id)?;
+  write_u16(writer, class.super_class_id)?;
+
+  // For now, tell that there are no interfaces
+  write_u16(writer, class.interfaces.len() as u16)?;
+  for interface_id in &class.interfaces {
+    write_u16(writer, *interface_id)?;
+  }
+  Ok(())
+}
+
+fn write_class_definition(writer: &mut Writer, class: &JavaClass) -> StdResult {
+  // TODO write the correct writer
+  // No fields
+  write_u16(writer, 0)?;
+
+  // Write methods
+  write_u16(writer, class.methods.len() as u16)?;
+  for method in &class.methods {
+    write_u16(writer, method.access)?;
+    write_u16(writer, method.name_index)?;
+    write_u16(writer, method.descriptor_index)?;
+    write_u16(writer, method.attributes.len() as u16)?;
+    for entry in &method.attributes {
+      write_attribute(writer, entry)?;
+    }
+  }
+
+  // No attributes
+  write_u16(writer, 0)
+}
+
+fn write_attribute(writer: &mut Writer, &(ref idx, ref attribute): &(u16, Attribute)) -> StdResult {
   match attribute {
     &Attribute::Code(ref max_stack, ref operations) => {
-      write_u16(file, *idx)?;
+      write_u16(writer, *idx)?;
 
-      let attribute_length: u32 = 1234; // TODO compute the correct length
-      write_u32(file, attribute_length)?;
-      write_u16(file, *max_stack)?;
+      let mut op_writer = VecWriter { data: Vec::new() };
+      for operation in operations {
+        write_operation(&mut op_writer, operation)?;
+      }
 
+      let mut attr_writer = VecWriter { data: Vec::new() };
+      write_u16(&mut attr_writer, *max_stack)?;
       let local_vars_count: u8 = operations.iter()
         .map(|op| match op {
           &Operation::aload(ref idx) => *idx,
@@ -155,71 +191,75 @@ fn write_attribute(file: &mut File, &(ref idx, ref attribute): &(u16, Attribute)
           _ => 0u8
         })
         .max().unwrap_or(0u8);
-      write_u16(file, local_vars_count as u16)?;
-      let code_length: u32 = 234; // TODO: compute the correct code length
-      write_u32(file, code_length)?;
-      for operation in operations {
-        write_operation(file, operation)?;
-      }
+      write_u16(&mut attr_writer, local_vars_count as u16)?;
+      write_u32(&mut attr_writer, op_writer.data.len() as u32)?;
+      attr_writer.write(&op_writer.data[..])?;
+
+      write_u32(writer, attr_writer.data.len() as u32)?;
+      writer.write(&attr_writer.data[..])?;
 
       // Not used so far
-      write_u16(file, 0)?; // No exception tables
-      write_u16(file, 0) // No attributes
+      write_u16(writer, 0)?; // No exception tables
+      write_u16(writer, 0) // No attributes
     }
   }
 }
 
-fn write_operation(file: &mut File, operation: &Operation) -> io::Result<()> {
+fn write_operation(writer: &mut Writer, operation: &Operation) -> StdResult {
   match operation {
     &Operation::aload(ref idx) => {
       // if idx > 3 { // TODO write the optimization
-      write_u8(file, 25)?;
-      write_u8(file, *idx)
+      write_u8(writer, 25)?;
+      write_u8(writer, *idx)
     },
     &Operation::astore(ref idx) => {
-      write_u8(file, 58)?;
-      write_u8(file, *idx)
+      write_u8(writer, 58)?;
+      write_u8(writer, *idx)
     },
     &Operation::iastore => {
-      write_u8(file, 79)
+      write_u8(writer, 79)
     },
     &Operation::iconst_1 => {
-      write_u8(file, 4)
+      write_u8(writer, 4)
     },
     &Operation::invokespecial(ref idx) => {
-      write_u8(file, 183)?;
-      write_u16(file, *idx)
+      write_u8(writer, 183)?;
+      write_u16(writer, *idx)
     },
     &Operation::invokevirtual(ref idx) => {
-      write_u8(file, 182)?;
-      write_u16(file, *idx)
+      write_u8(writer, 182)?;
+      write_u16(writer, *idx)
     },
     &Operation::ldc(ref idx) => {
       // Optimize using ldc or ldc_w
       if *idx < 256 {
-        write_u8(file, 18)?;
-        write_u8(file, (*idx & 0xff) as u8)
+        write_u8(writer, 18)?;
+        write_u8(writer, (*idx & 0xff) as u8)
       } else {
-        write_u8(file, 19)?;
-        write_u16(file, *idx)
+        write_u8(writer, 19)?;
+        write_u16(writer, *idx)
       }
     },
     &Operation::new(ref idx) => {
-      write_u8(file, 187)?;
-      write_u16(file, *idx)
+      write_u8(writer, 187)?;
+      write_u16(writer, *idx)
     },
     &Operation::newarray(ref array_type) => {
-      write_u8(file, 188)?;
-      write_u8(file, array_type.clone() as u8)
+      write_u8(writer, 188)?;
+      write_u8(writer, array_type.clone() as u8)
     }
   }
 }
 
-pub fn write(class: &JavaClass, output_file: &Path) -> io::Result<()> {
+pub fn write(class: &JavaClass, output_file: &Path) -> StdResult {
   // println!("class: {:?}", class);
-  let mut file = File::create(output_file)?;
-  write_header(&mut file)?;
-  write_constant_pool(&mut file, class)?;
-  write_class_info(&mut file, class)?;
-  write_class_definition(&mut file, class)
+  let mut writer = FileWriter { file: File::create(output_file)? };
+  write_header(&mut writer)?;
+  writer.flush()?;
+  write_constant_pool(&mut writer, class)?;
+  writer.flush()?;
+  write_class_info(&mut writer, class)?;
+  writer.flush()?;
+  write_class_definition(&mut writer, class)?;
+  writer.flush()
 }
